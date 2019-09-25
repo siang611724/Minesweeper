78a80a1ddcc3e2e3ee6895ec34ad4a3c8698fd57
Siang <wang710521@gmail.com>
2019-09-25 11:18:09 +0800

    second commit

---
+++ Platform/Admin/resources/views/home/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>Document</title>
+
+    <style>
+        .title {
+            font-size: 30px;
+            background-color: #323233;
+            color: white;
+            width: 100%;
+        }
+        .row {
+            position: relative;
+        }
+        .btn {
+            width:20vw;
+            height:50px;
+            
+        }
+    </style>
+
+</head>
+<body>
+    <nav class="nav">
+        <div class="title nav-link active">MinesweeperOnline</div>
+    </nav>
+    <div class="row">
+        <div class="col-4" style="background-color:gray; HEIGHT:100vh">
+            <button type="button" class="btn btn-secondary">會員</button><br>
+            <button type="button" class="btn btn-secondary">公告</button>
+        </div>
+        <div class="col-8">各種資訊</div>
+    </div>
+</body>
+</html>
