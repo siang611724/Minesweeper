d379475bd0654c41547b16b2b022363d7fdd7431
Siang <wang710521@gmail.com>
2019-10-04 17:53:44 +0800

    191004 test api

---
+++ Platform/Admin/app/Http/Controllers/api/AnnounceController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Announce;
+use App\Http\Resources\AnnounceResource;
+use App\Http\Resources\AnnounceResourceCollection;
+
+class AnnounceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ann = Announce::all();
+        return response()->json($ann);    
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ann = Announce::find($id);
+        $ann->title = $requset->title;
+        $ann->content = $requset->content;
+        $ann->save();
+        return response()->json($ann);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $ann = Announce::all();
+        $annDel = Announce::find($id);
+        $annDel->delete();
+        return response()->json($ann);
+    }
+}
